9a59a7129e159d022a8e172d13629374706aa23c
Tyler Ordenstein <tylerordenstein@gmail.com>
2019-09-21 17:27:24 -0700

    Got server up and running to display front end

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, jsonify, make_response, flash, session)
 
 
 app = Flask(__name__)
@@ -7,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug
+
+    app.run(port=5000, host='0.0.0.0')
